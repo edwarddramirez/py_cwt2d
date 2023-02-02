066692a4ae5e7f3c4b749eb6b84f37db10274a3e
edwarddramirez <93281510+edwarddramirez@users.noreply.github.com>
2023-02-02 11:15:43 -0500

    Update setup.py
    updated setup.py

--- setup.py
+++ setup.py
@@ -6,11 +6,11 @@ REQUIREMENTS = ['numpy',]
 
 setup(
     name='py_cwt2d',
-    version=0.1,
+    version=0.11,
     description='Two Dimensional Continuous Wavelet Transform',
     author='Leon Archer',
     author_email='na',
-    url='https://github.com/LeonArcher/py_cwt2d',
+    url='https://github.com/edwarddramirez/py_cwt2d',
     long_description=open('README.md').read(),
     packages=find_packages(),
     install_requires=REQUIREMENTS
